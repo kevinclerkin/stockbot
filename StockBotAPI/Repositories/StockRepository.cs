380a1b7ce5778eb53aad6e417d84c58be0fa4ab9
Kevin Clerkin <kevin.clerkin1@gmail.com>
2024-04-18 23:00:34 +0100

    Added stockrepository

---
+++ StockBotAPI/Repositories/StockRepository.cs
@@ -0,0 +1,18 @@
+﻿using StockBotAPI.Interfaces;
+using StockBotAPI.Models;
+
+namespace StockBotAPI.Repositories
+{
+    public class StockRepository : IStockRepository
+    {
+        public Task<List<Stock>> GetAsync()
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<Stock> GetByIdAsync(int id)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
