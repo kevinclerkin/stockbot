fdecf056429ef1edafe86706eb07a4d0d084492d
Kevin Clerkin <kevin.clerkin1@gmail.com>
2024-04-20 22:52:45 +0100

    Added identity roles to applicationdbcontext

--- StockBotAPI/Data/ApplicationDbContext.cs
+++ StockBotAPI/Data/ApplicationDbContext.cs
@@ -1,4 +1,5 @@
-﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+﻿using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore;
 using StockBotAPI.Models;
 
@@ -13,5 +14,29 @@ namespace StockBotAPI.Data
         }
 
         public DbSet<Stock> Stocks { get; set;}
+
+        protected override void OnModelCreating(ModelBuilder builder)
+        {
+            base.OnModelCreating(builder);
+
+            List<IdentityRole> roles = new List<IdentityRole> {
+                new IdentityRole
+                {
+                    Name = "Admin",
+                    NormalizedName = "ADMIN",
+
+                },
+
+                new IdentityRole
+                {
+                    Name = "User",
+                    NormalizedName = "USER"
+                }
+                
+                
+            };
+
+            builder.Entity<IdentityRole>().HasData(roles);
+        }
     }
 }
