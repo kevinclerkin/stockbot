420e6678bb02947b91fe182e8d77b5a651c9f482
Kevin Clerkin <kevin.clerkin1@gmail.com>
2024-04-20 22:16:14 +0100

    Added register user dto and user controller with register endpoint

---
+++ StockBotAPI/Controllers/AppUserController.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using StockBotAPI.Models;
+
+namespace StockBotAPI.Controllers
+{
+    [Route("api/user")]
+    [ApiController]
+    public class AppUserController : ControllerBase
+    {
+        private readonly UserManager<AppUser> _userManager;
+        public AppUserController(UserManager<AppUser> userManager)
+        {
+            _userManager = userManager;
+        }
+
+        [HttpPost("register")]
+
+        public async Task<IActionResult> Register([FromBody] RegisterDTO register)
+        {
+            return null;
+        }
+    }
+}
