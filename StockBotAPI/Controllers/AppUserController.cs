13758bd560e3cc2a2298b6e6b5d9d0c880e8506c
Kevin Clerkin <kevin.clerkin1@gmail.com>
2024-04-20 23:23:52 +0100

    Added register user and user role endpoint methods and validation

--- StockBotAPI/Controllers/AppUserController.cs
+++ StockBotAPI/Controllers/AppUserController.cs
@@ -20,7 +20,45 @@ namespace StockBotAPI.Controllers
 
         public async Task<IActionResult> Register([FromBody] RegisterDTO register)
         {
-            return null;
+            try
+            {
+                if (!ModelState.IsValid)
+                {
+                    return BadRequest(ModelState);
+                }
+
+                var appUser = new AppUser
+                {
+                    UserName = register.Username,
+
+                    Email = register.Email,
+                };
+
+                var registeredUser = await _userManager.CreateAsync(appUser, register.Password);
+
+                if (registeredUser.Succeeded)
+                {
+                    var assignedRole = await _userManager.AddToRoleAsync(appUser, "User");
+                    
+                    if (assignedRole.Succeeded)
+                    {
+                        return Ok("User Registered");
+                    }
+                    else
+                    {
+                        return BadRequest();
+                    }
+                }
+                else
+                {
+                    return StatusCode(500, registeredUser);
+                }
+            }
+            catch (Exception ex)
+            {
+                return StatusCode(500, 0);
+
+            }
         }
     }
 }
