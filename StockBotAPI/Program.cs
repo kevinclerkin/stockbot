d6748eb360eb6892cf66d7dbd890cd75878c2766
Kevin Clerkin <kevin.clerkin1@gmail.com>
2024-04-20 17:06:06 +0100

    Added istockrepository as a scoped service

--- StockBotAPI/Program.cs
+++ StockBotAPI/Program.cs
@@ -1,5 +1,7 @@
 using Microsoft.EntityFrameworkCore;
 using StockBotAPI.Data;
+using StockBotAPI.Interfaces;
+using StockBotAPI.Repositories;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -9,6 +11,7 @@ builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+builder.Services.AddScoped<IStockRepository, StockRepository>();
 
 builder.Services.AddDbContext<ApplicationDbContext>(options =>
 {
