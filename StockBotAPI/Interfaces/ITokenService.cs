9a88d2e57e0ec7aff77e6275d3787113e442f98d
Kevin Clerkin <kevin.clerkin1@gmail.com>
2024-04-21 14:39:53 +0100

    Added token interface and service with jwt token creation

---
+++ StockBotAPI/Interfaces/ITokenService.cs
@@ -0,0 +1,9 @@
+﻿using StockBotAPI.Models;
+
+namespace StockBotAPI.Interfaces
+{
+    public interface ITokenService
+    {
+        string CreateToken(AppUser appUser);
+    }
+}
---
+++ StockBotAPI/Services/TokenService.cs
@@ -0,0 +1,47 @@
+﻿using Microsoft.AspNetCore.Server.IIS.Core;
+using Microsoft.IdentityModel.Tokens;
+using StockBotAPI.Interfaces;
+using StockBotAPI.Models;
+using System.IdentityModel.Tokens.Jwt;
+using System.Security.Claims;
+using System.Text;
+
+namespace StockBotAPI.Services
+{
+    public class TokenService : ITokenService
+    {
+        private readonly IConfiguration _configuration;
+        private readonly SymmetricSecurityKey _securityKey;
+        public TokenService(IConfiguration configuration)
+        {
+            _configuration = configuration;
+            _securityKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(_configuration["JWT:SigningKey"]));
+            
+        }
+        public string CreateToken(AppUser appUser)
+        {
+            var claims = new List<Claim>
+            {
+                new Claim(JwtRegisteredClaimNames.Email, appUser.Email),
+                new Claim(JwtRegisteredClaimNames.GivenName, appUser.UserName)
+            };
+
+            var encrypt = new SigningCredentials(_securityKey, SecurityAlgorithms.HmacSha512Signature);
+
+            var tokenDesc = new SecurityTokenDescriptor
+            {
+                Subject = new ClaimsIdentity(claims),
+                Expires = DateTime.UtcNow.AddDays(7),
+                SigningCredentials = encrypt,
+                Issuer = _configuration["JWT:Issuer"],
+                Audience = _configuration["JWT:Audience"]
+            };
+
+            var tokenHandler = new JwtSecurityTokenHandler();
+
+            var token = tokenHandler.CreateToken(tokenDesc);
+
+            return tokenHandler.WriteToken(token);
+        }
+    }
+}
