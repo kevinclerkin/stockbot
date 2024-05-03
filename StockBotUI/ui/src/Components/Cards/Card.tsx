5605890ad3d7dfcd45d724977181f79fb2f87286
Kevin Clerkin <kevin.clerkin1@gmail.com>
2024-05-03 23:39:07 +0100

    Modified card styling and layout

--- StockBotUI/ui/src/Components/Cards/Card.tsx
+++ StockBotUI/ui/src/Components/Cards/Card.tsx
@@ -12,15 +12,22 @@ interface Props {
 
 const Card = ({id, searchResult, onPortfolioAdd}: Props) => {
   return (
-    <div className="card">
-      <div className="card-header">
-        <h2>Stock</h2>
-      </div>
-      <div className="card-body">
-        <h3>{searchResult.name}</h3>
-        <p>${searchResult.stockExchange}</p>
-        <AddPortfolio onPortfolioAdd={onPortfolioAdd} symbol={searchResult.symbol} />
-      </div>
+    <div
+      className="flex flex-col items-center justify-between w-full p-6 bg-slate-100 rounded-lg md:flex-row"
+      key={id}
+      id={id}
+    >
+      <h2 className="font-bold text-center text-veryDarkViolet md:text-left">
+        {searchResult.name} ({searchResult.symbol})
+      </h2>
+      <p className="text-veryDarkBlue">{searchResult.currency}</p>
+      <p className="font-bold text-veryDarkBlue">
+        {searchResult.exchangeShortName} - {searchResult.stockExchange}
+      </p>
+      <AddPortfolio
+        onPortfolioAdd={onPortfolioAdd}
+        symbol={searchResult.symbol}
+      />
     </div>
   )
 }
