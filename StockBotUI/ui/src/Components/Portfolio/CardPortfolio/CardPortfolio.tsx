d0a8912748abeda00bb0be0dc28d6a35dfb99f71
Kevin Clerkin <kevin.clerkin1@gmail.com>
2024-05-04 00:06:56 +0100

    Modified card portfolio component styling

--- StockBotUI/ui/src/Components/Portfolio/CardPortfolio/CardPortfolio.tsx
+++ StockBotUI/ui/src/Components/Portfolio/CardPortfolio/CardPortfolio.tsx
@@ -9,10 +9,13 @@ interface Props {
 
 const CardPortfolio = ({portfolioItem, onPortfolioRemove}: Props) => {
   return (
-    <>
-    <div>{portfolioItem}</div>
-    <RemovePortfolio onPortfolioRemove={onPortfolioRemove} portfolioItem={portfolioItem} />
-    </>
+    <div className="flex flex-col w-full p-8 space-y-4 text-center rounded-lg shadow-lg md:w-1/3">
+    <p className="pt-6 text-xl font-bold">{portfolioItem}</p>
+    <RemovePortfolio
+      portfolioItem={portfolioItem}
+      onPortfolioRemove={onPortfolioRemove}
+    />
+  </div>
   )
 }
 
