c363b59f3fbcd3e9c7aa1ce7335d46b30bb76e8c
Kevin Clerkin <kevin.clerkin1@gmail.com>
2024-05-03 23:57:09 +0100

    Modified portfolio list component styling

--- StockBotUI/ui/src/Components/Portfolio/PortfolioList/PortfolioList.tsx
+++ StockBotUI/ui/src/Components/Portfolio/PortfolioList/PortfolioList.tsx
@@ -9,12 +9,29 @@ interface Props {
 
 const PortfolioList = ({portfolio, onPortfolioRemove}: Props) => {
   return (
-    <div>
-        <h3>Portfolio</h3>
-        {portfolio && portfolio.map((portfolioItem) => {
-            return <CardPortfolio portfolioItem={portfolioItem} onPortfolioRemove={onPortfolioRemove}/>
-        })}
-    </div>
+    <section id="portfolio">
+      <h2 className="mb-3 mt-3 text-3xl font-semibold text-center md:text-4xl">
+        My Portfolio
+      </h2>
+      <div className="relative flex flex-col items-center max-w-5xl mx-auto space-y-10 px-10 mb-5 md:px-6 md:space-y-0 md:space-x-7 md:flex-row">
+        <>
+          {portfolio.length > 0 ? (
+            portfolio.map((portfolioItem) => {
+              return (
+                <CardPortfolio
+                  portfolioItem={portfolioItem}
+                  onPortfolioRemove={onPortfolioRemove}
+                />
+              );
+            })
+          ) : (
+            <h3 className="mb-3 mt-3 text-xl font-semibold text-center md:text-xl">
+              No Portfolio.
+            </h3>
+          )}
+        </>
+      </div>
+    </section>
   )
 }
 
