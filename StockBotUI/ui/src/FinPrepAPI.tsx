47eb8c020d1c5d0bf9a711c61aacce9d45a8abc9
Kevin Clerkin <kevin.clerkin1@gmail.com>
2024-05-04 21:50:57 +0100

    Added get company profile api call and get stock profile when stock page loads

--- StockBotUI/ui/src/FinPrepAPI.tsx
+++ StockBotUI/ui/src/FinPrepAPI.tsx
@@ -27,7 +27,7 @@ export const getCompanyProfile = async (query: string)  => {
         const data = await axios.get<CompanyProfile[]>(`https://financialmodelingprep.com/api/v3/profile/${query}?apikey=${process.env.REACT_APP_API_KEY}`);
         return data;
     }
-    catch (error) {
+    catch (error: any) {
         if(axios.isAxiosError(error)) {
             console.log(error.message);
             return error.message;
--- StockBotUI/ui/src/Pages/Stock/Stock.tsx
+++ StockBotUI/ui/src/Pages/Stock/Stock.tsx
@@ -1,10 +1,31 @@
 import React from 'react'
+import { useParams } from 'react-router-dom'
+import { CompanyProfile } from '../../company';
+import { getCompanyProfile } from '../../FinPrepAPI';
+import { AxiosResponse } from 'axios';
 
 interface Props {}
 
 const Stock = (props: Props) => {
+  let {ticker} = useParams();
+  const [company, setCompany] = React.useState<CompanyProfile>();
+
+  React.useEffect(() => {
+    const getStockProfileInit = async () => {
+      const result = await getCompanyProfile(ticker!) as AxiosResponse<CompanyProfile[], any>;
+      setCompany(result?.data[0]);
+    }
+    getStockProfileInit();
+  }, [])
   return (
-    <div>Stock</div>
+    <>
+    { company ? (
+    <div>{company?.companyName} {company?.exchange} {company?.mktCap}</div>
+    ) : (
+      <div>Company or Security Not Found</div>
+
+    )}
+    </>
   )
 }
 
