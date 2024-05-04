ae6e7e7f7f7b955afaf2c123887949e6c53588b3
Kevin Clerkin <kevin.clerkin1@gmail.com>
2024-05-04 20:00:00 +0100

    Added getcompanyprofile method to call profiles from api

--- StockBotUI/ui/src/FinPrepAPI.tsx
+++ StockBotUI/ui/src/FinPrepAPI.tsx
@@ -1,5 +1,5 @@
 import axios, { AxiosResponse } from "axios";
-import { Company } from "./company";
+import { Company, CompanyProfile } from "./company";
 
 interface searchResponse {
     data: Company[];
@@ -21,3 +21,20 @@ export const searchCompany = async (query: string): Promise<string | AxiosRespon
         }
     }
 };
+
+export const getCompanyProfile = async (query: string)  => {
+    try{
+        const data = await axios.get<CompanyProfile[]>(`https://financialmodelingprep.com/api/v3/profile/${query}?apikey=${process.env.REACT_APP_API_KEY}`);
+        return data;
+    }
+    catch (error) {
+        if(axios.isAxiosError(error)) {
+            console.log(error.message);
+            return error.message;
+        }
+        else{
+            console.log(error);
+            return error as string;
+        }
+    }
+}
