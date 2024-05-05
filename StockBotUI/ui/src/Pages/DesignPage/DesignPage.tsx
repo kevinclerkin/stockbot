bb7ad903a37f808e1cca23faa7ea2d82847f61a8
Kevin Clerkin <kevin.clerkin1@gmail.com>
2024-05-05 22:39:55 +0100

    Tested test data by returning metrics and table to design page

--- StockBotUI/ui/src/Pages/DesignPage/DesignPage.tsx
+++ StockBotUI/ui/src/Pages/DesignPage/DesignPage.tsx
@@ -1,10 +1,27 @@
-interface Props {
+import React from "react";
+import { TestDataCompany } from "../../Components/Table/TableData";
+import MetricsList from "../../Components/MetricsList/MetricsList";
+import Table from "../../Components/Table/Table";
+
+type Props = {
   
 }
+
+const data = TestDataCompany;
+
+const tableConfig = [
+  {
+    label: "symbol",
+    render: (company: any) => company.symbol,
+  },
+];
 
 const DesignPage = (props: Props) => {
   return (
     <>
+    
+      <MetricsList config={tableConfig} data={data} />
+      <Table config={tableConfig} data={data} />
     
     </>
   )
