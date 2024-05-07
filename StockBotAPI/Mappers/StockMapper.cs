5b78963012f70b7775aaf8c9ab53157e84c42e5c
Kevin Clerkin <kevin.clerkin1@gmail.com>
2024-05-07 22:28:08 +0100

    Created stock dto properties and a mapper to create stock dtos

---
+++ StockBotAPI/Mappers/StockMapper.cs
@@ -0,0 +1,31 @@
+﻿using StockBotAPI.DTO;
+using StockBotAPI.Models;
+
+namespace StockBotAPI.Mappers
+{
+    public static class StockMapper
+    {
+        public static StockDTO ToStockDTO(this Stock stock)
+        {
+
+            return new StockDTO
+            {
+                Id = stock.Id,
+
+                Symbol = stock.Symbol,
+
+                CompanyName = stock.CompanyName,
+
+                Purchase = stock.Purchase,
+
+                LastDiv = stock.LastDiv,
+
+                Industry = stock.Industry,
+
+                MarketCap = stock.MarketCap
+
+            };
+
+        }
+    }
+}
