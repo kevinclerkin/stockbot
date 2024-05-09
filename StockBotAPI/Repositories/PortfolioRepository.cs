8e16213bfcad0c021951ccf499c9ad977e60ef61
Kevin Clerkin <kevin.clerkin1@gmail.com>
2024-05-09 22:51:51 +0100

    Added portfolio repo and get portfolio method

---
+++ StockBotAPI/Repositories/PortfolioRepository.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.EntityFrameworkCore;
+using StockBotAPI.Data;
+using StockBotAPI.Interfaces;
+using StockBotAPI.Models;
+
+namespace StockBotAPI.Repositories
+{
+    public class PortfolioRepository : IPortfolioRepository
+    {
+        private readonly ApplicationDbContext _context;
+        public PortfolioRepository(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+        public async Task<List<Stock>> GetUserPortfolio(AppUser user)
+        {
+            return await _context.Portfolios.Where(u => u.AppUserId == user.Id)
+                .Select(stock => new Stock
+                {
+                    Id = stock.StockId,
+                    Symbol = stock.Stock.Symbol,
+                    CompanyName = stock.Stock.CompanyName,
+                    Purchase = stock.Stock.Purchase,
+                    LastDiv = stock.Stock.LastDiv,
+                    Industry = stock.Stock.Industry,
+                    MarketCap = stock.Stock.MarketCap
+                }).ToListAsync();
+        }
+    }
+}
