e9eaba6c3734229a9b16bf385cdebd7c6d951697
Kevin Clerkin <kevin.clerkin1@gmail.com>
2024-05-09 15:39:14 +0100

    Added foreign keys and portfolio entity to db context

--- StockBotAPI/Data/ApplicationDbContext.cs
+++ StockBotAPI/Data/ApplicationDbContext.cs
@@ -15,9 +15,23 @@ namespace StockBotAPI.Data
 
         public DbSet<Stock> Stocks { get; set;}
 
+        public DbSet<Portfolio> Portfolios { get; set;}
+
         protected override void OnModelCreating(ModelBuilder builder)
         {
             base.OnModelCreating(builder);
+
+            builder.Entity<Portfolio>(x => x.HasKey(a => new { a.AppUserId, a.StockId }));
+
+            builder.Entity<Portfolio>()
+                .HasOne(u => u.AppUser)
+                .WithMany(u => u.Portfolio)
+                .HasForeignKey(a => a.AppUserId);
+
+            builder.Entity<Portfolio>()
+                .HasOne(u => u.Stock)
+                .WithMany(u => u.Portfolio)
+                .HasForeignKey(a => a.StockId);
 
             List<IdentityRole> roles = new List<IdentityRole> {
                 new IdentityRole
