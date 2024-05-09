c8d67ef4cbce32e6c48c19e2e9682cd57d4c2e62
Kevin Clerkin <kevin.clerkin1@gmail.com>
2024-05-09 22:26:34 +0100

    Created portfolio controller and extension method to retrieve user claims

---
+++ StockBotAPI/Controllers/PortfolioController.cs
@@ -0,0 +1,37 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using StockBotAPI.Extensions;
+using StockBotAPI.Interfaces;
+using StockBotAPI.Models;
+
+namespace StockBotAPI.Controllers
+{
+    [Route("api/portfolio")]
+    [ApiController]
+    public class PortfolioController : ControllerBase
+    {
+        private readonly UserManager<AppUser> _userManager;
+
+        private readonly IStockRepository _stockRepository;
+        public PortfolioController(UserManager<AppUser> userManager, IStockRepository stockRepository)
+        {
+            _userManager = userManager;
+
+            _stockRepository = stockRepository;
+            
+        }
+
+        [HttpGet]
+        [Authorize]
+        public async Task<IActionResult> GetPortfolio()
+        {
+            var userName = User.GetUser();
+            var appUser = await _userManager.FindByEmailAsync(userName);
+
+            return Ok();
+
+        }
+    }
+}
---
+++ StockBotAPI/Extensions/ClaimsExtensions.cs
@@ -0,0 +1,13 @@
+﻿using System.Runtime.CompilerServices;
+using System.Security.Claims;
+
+namespace StockBotAPI.Extensions
+{
+    public static class ClaimsExtensions
+    {
+        public static string GetUser(this ClaimsPrincipal user)
+        {
+            return user.Claims.SingleOrDefault(x => x.Type.Equals("https://openid.net/specs/openid-connect-core-1_0.html#StandardClaims")).Value;
+        }
+    }
+}
