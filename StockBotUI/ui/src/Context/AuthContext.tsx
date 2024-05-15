54aba16a4e636b06e8d5cf4e822f928d29e556ec
Kevin Clerkin <kevin.clerkin1@gmail.com>
2024-05-15 18:52:55 +0100

    Add user model with user profile type

--- StockBotUI/ui/src/Context/AuthContext.tsx
+++ StockBotUI/ui/src/Context/AuthContext.tsx
@@ -1,4 +1,5 @@
 import React, { createContext } from "react";
+import { UserProfile } from "../Models/User";
 
 
 type UserContextType = {
---
+++ StockBotUI/ui/src/Models/User.tsx
@@ -0,0 +1,5 @@
+export type UserProfile = {
+    username: string;
+    email: string;
+    
+}
