e5a35036cd736f79375502146283e798e19b44ae
Kevin Clerkin <kevin.clerkin1@gmail.com>
2024-05-16 16:59:23 +0100

    Add user provider in app

--- StockBotUI/ui/src/App.tsx
+++ StockBotUI/ui/src/App.tsx
@@ -1,13 +1,16 @@
 import './App.css';
 import NavBar from './Components/NavBar/NavBar';
 import { Outlet } from 'react-router';
+import { UserProvider } from './Context/AuthContext';
 
 function App() {
   
   return (
     <div className="App">
+      <UserProvider>
       <NavBar />
       <Outlet />
+      </UserProvider>
     </div>
   );
 }
