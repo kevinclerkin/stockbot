1bf66a46d443ee597cc0065585caa3232f081432
Kevin Clerkin <kevin.clerkin1@gmail.com>
2024-05-17 20:53:52 +0100

    Add get and delete functions to portfolio service

--- StockBotUI/ui/src/Services/PortfolioService.tsx
+++ StockBotUI/ui/src/Services/PortfolioService.tsx
@@ -1,7 +1,19 @@
 import axios from "axios"
-import { PostPortfolio } from "../Models/Portfolio"
+import { GetPortfolio, PostPortfolio } from "../Models/Portfolio"
 
 const API = "https://localhost:7297/api"
+
+
+export const GetPortfolioFromAPI = async () => {
+    try {
+        const data = await axios.get<GetPortfolio[]>(API)
+        return data
+    }
+    catch(error) {
+        console.error(error)
+    }
+}
+
 
 export const AddPortfolioFromAPI = async (symbol: string) => {
     try {
@@ -12,3 +24,15 @@ export const AddPortfolioFromAPI = async (symbol: string) => {
         console.error(error)
     }
 }
+
+
+export const DeletePortfolioFromAPI = async (symbol: string) => {
+    try {
+        const data = await axios.delete<PostPortfolio>(`${API}/portfolio/?symbol=${ symbol }`)
+        return data
+    }
+    catch(error) {
+        console.error(error)
+    }
+}
+
