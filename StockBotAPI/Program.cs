b4afab14a9267525b412fe47e1932e0a699166d7
Kevin Clerkin <kevin.clerkin1@gmail.com>
2024-07-04 20:20:13 +0100

    Add fin prep service as scoped service

--- StockBotAPI/Program.cs
+++ StockBotAPI/Program.cs
@@ -49,6 +49,7 @@ builder.Services.AddSwaggerGen(option =>
 builder.Services.AddScoped<IStockRepository, StockRepository>();
 builder.Services.AddScoped<IPortfolioRepository, PortfolioRepository>();
 builder.Services.AddScoped<ITokenService, TokenService>();
+builder.Services.AddScoped<IFinPrepService, FinPrepService>();
 
 builder.Services.AddDbContext<ApplicationDbContext>(options =>
 {
