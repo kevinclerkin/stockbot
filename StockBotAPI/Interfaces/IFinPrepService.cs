1754120fba369868c29363e0fad57f1d373397b0
Kevin Clerkin <kevin.clerkin1@gmail.com>
2024-07-05 15:50:26 +0100

    Add get stock profile to fin prep service

--- StockBotAPI/Interfaces/IFinPrepService.cs
+++ StockBotAPI/Interfaces/IFinPrepService.cs
@@ -5,5 +5,7 @@ namespace StockBotAPI.Interfaces
     public interface IFinPrepService
     {
         Task<string> GetStockBySymbol(string symbol);
+
+        Task<FinPrepDTO> GetStockProfile(string symbol);
     }
 }
--- StockBotAPI/Services/FinPrepService.cs
+++ StockBotAPI/Services/FinPrepService.cs
@@ -49,7 +49,37 @@ namespace StockBotAPI.Services
                 return null;
             }
         }
-        
-        
+
+        public async Task<FinPrepDTO> GetStockProfile(string symbol)
+        {
+            try
+            {
+                var apiKey = _configuration["FMPKey"];
+                if (string.IsNullOrEmpty(apiKey))
+                {
+                    throw new InvalidOperationException("API key for Financial Modeling Prep is missing.");
+                }
+
+                var requestUrl = $"https://financialmodelingprep.com/api/v3/profile/{symbol}?apikey={apiKey}";
+                var result = await _httpClient.GetAsync(requestUrl);
+
+                if (result.IsSuccessStatusCode)
+                {
+                    var content = await result.Content.ReadAsStringAsync();
+                    var stocks = JsonSerializer.Deserialize<FinPrepDTO[]>(content);
+                    return stocks?.FirstOrDefault();
+                }
+                else
+                {
+                    Console.WriteLine($"Error: {result.StatusCode}");
+                    return null;
+                }
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine($"Exception: {e.Message}");
+                return null;
+            }
+        }
     }
 }
