efb58a043ce84dd60258af2f25c0001da9c8a041
Kevin Clerkin <kevin.clerkin1@gmail.com>
2024-07-22 21:43:07 +0100

    Add in memory database for stock repo tests

---
+++ StockBotTests/Repositories/StockRepositoryTests.cs
@@ -0,0 +1,47 @@
+﻿using Microsoft.EntityFrameworkCore;
+using StockBotAPI.Data;
+using StockBotAPI.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StockBotTests.Repositories
+{
+    public class StockRepositoryTests
+    {
+        private async Task<ApplicationDbContext> GetDbContext()
+        {
+            var options = new DbContextOptionsBuilder<ApplicationDbContext>()
+                .UseInMemoryDatabase(databaseName: Guid.NewGuid().ToString())
+                .Options;
+
+            var databaseContext = new ApplicationDbContext(options);
+            databaseContext.Database.EnsureCreated();
+            if(await databaseContext.Stocks.CountAsync() <= 0)
+            {
+                for(int i=0; i<10; i++)
+                {
+                    databaseContext.Stocks.Add(
+                        new Stock()
+                        {
+                            Id = i,
+                            Symbol = "AAPL",
+                            CompanyName = "Apple",
+                            MarketCap = 1000000,
+                            LastDiv = 1,
+                            Industry = "Tech",
+                            Portfolio = new List<Portfolio>(),
+                            Purchase = 244
+                        }
+                        );
+                    await databaseContext.SaveChangesAsync();
+                }
+                
+            }
+            return databaseContext;
+        }
+    }
+}
